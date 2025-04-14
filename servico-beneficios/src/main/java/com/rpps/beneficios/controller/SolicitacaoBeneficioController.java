b0c65c243fd2e261080399ebc7e6560649480ea4
juliameireles <juliameirelespessoal@gmail.com>
2025-04-14 09:29:19 -0300

    adicionando readme

--- servico-beneficios/src/main/java/com/rpps/beneficios/controller/SolicitacaoBeneficioController.java
+++ servico-beneficios/src/main/java/com/rpps/beneficios/controller/SolicitacaoBeneficioController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
